727c39476f8dfb5ee31e5a48edb5dfeef8c344a5
SayoniPanditCN <sayoni@capitalnumbers.com>
2022-08-01 18:38:56 +0530

    Day2 Done

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <AllRoutes/> */}
-        <Form/>
+        <AllRoutes/>
       </header>
     </div>
   );
